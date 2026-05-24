0321ef34fb8590dc050c631e18b0df3f04e97e27
Andrew Baxter <heythisisandrewb@gmail.com>
2026-05-24 07:36:08 -0700

    add real social links to site.js

--- src/data/site.js
+++ src/data/site.js
@@ -7,8 +7,14 @@ export const site = {
   basePath: '/BeUnconventionalHQ/',
   image: 'https://heythisisandrew.github.io/BeUnconventionalHQ/banner.png',
   socials: {
-    youtube: '',
-    instagram: '',
-    x: '',
+    youtube: 'https://www.youtube.com/@BeUnconventionalHQ',
+    instagram: 'https://www.instagram.com/beunconventionalhq',
+    tiktok: 'https://www.tiktok.com/@beunconventional',
+    threads: 'https://www.threads.com/@beunconventionalhq',
+    facebook: 'https://www.facebook.com/profile.php?id=61557980693320',
+    substack: 'https://beunconventionalhq.substack.com/',
+    twitter: 'https://x.com/beunconhq',
+    letterboxd: 'https://letterboxd.com/beunconhq/',
+    amazon: 'https://www.amazon.com/shop/influencer-0931c541',
   },
 };
