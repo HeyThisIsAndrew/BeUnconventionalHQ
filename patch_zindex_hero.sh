9b7cc0ab5134fe863db2b678a38a05377f6ee497
Andrew Baxter <heythisisandrewb@gmail.com>
2026-09-06 23:27:02 -0700

    chore: remove scratch scripts

--- patch_zindex_hero.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in src/components/EventAnnouncement.astro src/components/EventFeatured.astro; do
-  # Replace top: 0; with top: 62px;
-  sed -i '' 's/top: 0;/top: 62px;/g' "$file"
-
-  # Replace the min-height and padding for the shrunk overlay
-  sed -i '' 's/min-height: 140px !important;/min-height: 80px !important;/g' "$file"
-  sed -i '' 's/padding-top: 80px;/padding-top: 0.5rem;/g' "$file"
-done
--- revert_zindex_hero.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in src/components/EventAnnouncement.astro src/components/EventFeatured.astro; do
-  sed -i '' 's/top: 62px;/top: 0;/g' "$file"
-  sed -i '' 's/min-height: 80px !important;/min-height: 140px !important;/g' "$file"
-  sed -i '' 's/padding-top: 0.5rem;/padding-top: 72px;/g' "$file"
-done
