9c8519f944d355865f05379c3191216ef2eaee9e
Andrew Baxter <heythisisandrewb@gmail.com>
2026-09-07 17:38:52 -0700

    fix(ci): fix uppercase .PNG imports in categories.js and remove unused ts warning

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,6 +1,6 @@
-import movieImg from '../assets/categories/movies.PNG';
-import tvImg from '../assets/categories/tv.PNG';
-import gamingImg from '../assets/categories/gaming.PNG';
+import movieImg from '../assets/categories/movies.png';
+import tvImg from '../assets/categories/tv.png';
+import gamingImg from '../assets/categories/gaming.png';
 import eventsImg from '../assets/categories/events.png';
 
 /**
